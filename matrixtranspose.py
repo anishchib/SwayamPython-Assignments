82b28a7e068059c89152c50579323b0327d7d854
anish chib <88385444+anishchib@users.noreply.github.com>
2021-08-12 20:58:20 +0530

    Update matrixtranspose.py

--- matrixtranspose.py
+++ matrixtranspose.py
@@ -1,12 +1,16 @@
-def matrixTranspose(anArray):
-  transposed = [None]*len(anArray[0])
+def transpose(anArray):
+    transposed = [None] * len(anArray[0])
+    # print(transposed)
+    # print(len(transposed))
+    # print(len(anArray))
 
-  for i in range(len(transposed)):
-    transposed[i] = [None]*len(transposed)
-
-  for t in range(len(anArray)):
-    for tt in range(len(anArray[t])):
-        transposed[t][tt] = anArray[tt][t]
-  return transposed
-
-theArray = [['a','b','c'],['d','e','f'],['g','h','i']]
+    for i in range(len(transposed)):
+        # print(i)
+        transposed[i] = [None] * len(anArray)
+    # print(transposed)
+    # print(len(anArray))
+    for t in range(len(transposed)):
+        for tt in range(len(anArray)):
+            # print(t,tt,tt,t)
+            transposed[t][tt] = anArray[tt][t]
+    return transposed
